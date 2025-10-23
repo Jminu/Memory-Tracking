332eae5ee2a4cf25ffa8cf65527f0e46f270442a
JinMinu <s9430939@naver.com>
2025-10-23 23:31:56 +0900

    Makefile 작성

---
+++ MemoryTracker/include/user_netlink_hook.h
@@ -0,0 +1,11 @@
+#ifndef USER_NETLINK_HOOK_H
+#define USER_NETLINK_HOOK_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+
+void send_registration(int nl_socket_fd, struct sockaddr_nl *src_addr);
+void set_nl_socket();
+
+#endif
