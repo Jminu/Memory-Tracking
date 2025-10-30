b70b1d4c8a98bb2106c16efcb6218fb22eb7fd75
Jin Minu <s9430939@naver.com>
2025-10-30 19:15:59 +0900

    brk, mmap, munmap 발생 시 유저단으로 pid보내기

--- linux/mm/mmap.c
+++ linux/mm/mmap.c
@@ -213,6 +213,9 @@ SYSCALL_DEFINE1(brk, unsigned long, brk)
 	if (mm->def_flags & VM_LOCKED)
 		populate = true;
 
+	/*
+	 *	시스템이 부팅완료된 후, brk 호출 시 유저 단으로 current->pid 전송
+	 */
 	if (system_state == SYSTEM_RUNNING) {
 		nl_send_msg(cur_pid);
 	}
@@ -559,6 +562,10 @@ unsigned long ksys_mmap_pgoff(unsigned long addr, unsigned long len,
 	}
 
 	retval = vm_mmap_pgoff(file, addr, len, prot, flags, pgoff);
+
+	/*
+	 *	시스템이 부팅완료된 후, mmap 호출 시 유저 단으로 current->pid 전송
+	 */
 	if (system_state == SYSTEM_RUNNING) {
 		nl_send_msg(current->pid);
 	}
@@ -1659,6 +1666,14 @@ static int __vm_munmap(unsigned long start, size_t len, bool unlock)
 		mmap_write_unlock(mm);
 
 	userfaultfd_unmap_complete(mm, &uf);
+
+	/*
+	 *	시스템이 부팅완료된 후, munmap 호출 시 유저 단으로 current->pid 전송
+	 */
+	if (system_state == SYSTEM_RUNNING) {
+		nl_send_msg(current->pid);
+	}
+
 	return ret;
 }
 
