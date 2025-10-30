e137a8f31d30a87551620097445a5abc4ffa5369
Jin Minu <s9430939@naver.com>
2025-10-30 23:04:06 +0900

    netlinkhook 헤더수정

--- linux/include/net/netlink_hook.h
+++ linux/include/net/netlink_hook.h
@@ -4,7 +4,7 @@
 #include <linux/types.h>
 #include <linux/skbuff.h>
 
-extern void nl_send_msg(pid_t pid);
+extern void nl_send_msg(pid_t pid, const char *syscall_name);
 extern void nl_recv_msg(struct sk_buff *skb);
 
 #endif
